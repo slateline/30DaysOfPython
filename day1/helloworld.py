9a956a462f90dc1bc87717640bbb159f2eaaab90
chungtruong5330 <150117545+chungtruong5330@users.noreply.github.com>
2026-09-09 22:41:38 -0700

    Initial commit

---
+++ day1/helloworld.py
@@ -0,0 +1,25 @@
+import math
+
+print(3+4)
+print(3-4)
+print(3*4)
+print(3%4)
+print(3/4)
+print(3**4)
+print(3//4)
+
+print("Chung")
+print("Truong")
+print("Canada")
+print("I am enjoying 30 days of python")
+
+print(type(10))
+print(type(9.8))
+print(type(3.14))
+print(type(4-4j))
+print(type(['Asabeneh', 'Python', 'Finland']))
+print(type('Chung'))
+print(type('Truong'))
+print(type('Canada'))
+
+print(math.sqrt((2-10)**2+(3-8)**2))
